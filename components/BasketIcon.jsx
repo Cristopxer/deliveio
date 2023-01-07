5497a48b3f55827637ee4f589a5a21df18df6174
cristopxer <cristopherortiz457@gmail.com>
2023-01-07 12:59:10 -0600

    Basket preview pop-up completed

---
+++ components/BasketIcon.jsx
@@ -0,0 +1,35 @@
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { formatCurrency } from "react-native-format-currency";
+import { useSelector } from "react-redux";
+import { selectBasketItems, selectBasketTotal } from "../features/basketSlice";
+
+const BasketIcon = () => {
+  const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBasketTotal);
+
+  const navigation = useNavigation();
+
+  const basketTotalFormatted = formatCurrency({
+    amount: basketTotal,
+    code: "USD",
+  });
+
+  return (
+    <View className="absolute bottom-5 w-full z-50">
+      <TouchableOpacity className="bg-[#00CCBB] mx-5 p-4 rounded-lg flex-row items-center space-x-1">
+        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">
+          {items.length}
+        </Text>
+        <Text className="flex-1 text-white font-extrabold text-lg text-center">
+          View Basket
+        </Text>
+        <Text className="text-lg text-white font-extrabold">
+          {basketTotalFormatted[0]}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BasketIcon;
