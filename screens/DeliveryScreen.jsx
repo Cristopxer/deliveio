6c8999a7d028b37d72f0ecdd024a99d2be2266ce
cristopxer <cristopherortiz457@gmail.com>
2023-01-18 18:54:07 -0600

    tweaks prev deploy

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -67,7 +67,7 @@ const DeliveryScreen = () => {
         />
       </MapView>
 
-      <SafeAreaView className="bg-white flex-row items-center space-x-3 h-28">
+      <SafeAreaView className="bg-white flex-row items-center space-x-3 h-24">
         <Image
           source={{ uri: "https://links.papareact.com/wru" }}
           className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
